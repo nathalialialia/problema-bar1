5aa4587fc5b97e5f86791098cc1a361c1ad05dc0
nathalialialia <nathaliasgds@gmail.com>
2020-05-07 23:33:40 -0300

    Leitura dos dados

--- main.cpp
+++ main.cpp
@@ -5,7 +5,20 @@
 int main() {
 	
 	
+	string sexo;
+	int cerv, refr, espeto;
+	double couvert, consumo, total,ingresso ;
 	
+	
+	cout << "Sexo: " << endl;
+	cin >> sexo;
+	cout << "Quantidade de cervejas: " << endl;	
+	cin >> cerv;
+	cout << "Quantidade de refrigerantes: " << endl;
+	cin >> refr;
+	cout << "Quantidade de espetinhos: " << endl;
+	cin >> espeto;
+
 	
 	
 	
