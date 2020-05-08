df79c4a49177e246f40c7bb6374bc497ce99b662
nathalialialia <nathaliasgds@gmail.com>
2020-05-07 23:36:17 -0300

    Relatorio gerado

--- main.cpp
+++ main.cpp
@@ -18,12 +18,27 @@ int main() {
 	cin >> refr;
 	cout << "Quantidade de espetinhos: " << endl;
 	cin >> espeto;
+	if (sexo == "F"){
+    ingresso = 8.0;
+}
+else {
+    ingresso = 10.0;}
+	consumo = cerv * 5.0 + refr * 3.0 + espeto * 7.0;
+	if (consumo > 30.0) {
+    couvert = 0.0;}	else{
+    couvert = 4.0;}
+	total = ingresso + consumo +couvert;
+	cout << fixed << setprecision(2);
+	cout << endl << "RELATORIO:" << endl;
+	cout << "Consumo = R$ " << consumo << endl;
+	if (couvert == 0.0){
+    cout << "Isento de Couvert" << endl;
+}	else{
+    cout << "Couvert = R$ " << couvert << endl;
+}
+cout << "Ingresso = R$" << ingresso << endl;
+cout << endl << "Valor a pagar = R$ " << total << endl;
 
-	
-	
-	
-	
-	
 	
 	
 	return 0;
